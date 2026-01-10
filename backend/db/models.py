b7a0f790a50529ffadee4d236d4523b2f1490537
HavilMal <aberetm@onet.eu>
2026-01-10 18:25:31 +0100

    created race repository

--- backend/db/models.py
+++ backend/db/models.py
@@ -18,58 +18,101 @@ class Base(DeclarativeBase):
 
 
 class Runner(Base):
-    __tablename__ = 'runners'
+    __tablename__ = "runners"
     id: Mapped[int] = mapped_column(primary_key=True)
     rfid_uid: Mapped[str] = mapped_column(String(30))  # todo check
     name: Mapped[str] = mapped_column(String(30))
     surname: Mapped[str] = mapped_column(String(30))
 
-    race_runners: Mapped[List["RaceRunner"]] = relationship(back_populates="runner")
+    race_runners: Mapped[List["RaceRunner"]] = relationship(
+        "RaceRunner",
+        back_populates="runner",
+        cascade="all, delete-orphan",
+    )
 
 
 class Checkpoint(Base):
-    __tablename__ = 'checkpoints'
+    __tablename__ = "checkpoints"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
 
-    race_checkpoints: Mapped[List["RaceCheckpoint"]] = relationship("RaceCheckpoint", back_populates="checkpoint")
-
-
-class Event(Base):
-    __tablename__ = 'events'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    runner_id: Mapped[id] = mapped_column(ForeignKey('runners.id'))
-    checkpoint_id: Mapped[int] = mapped_column(ForeignKey('checkpoints.id'))
-    race_id: Mapped[int] = mapped_column(ForeignKey('races.id'))
-    timestamp: Mapped[datetime] = mapped_column(DateTime)
+    race_checkpoints: Mapped[List["RaceCheckpoint"]] = relationship(
+        "RaceCheckpoint",
+        back_populates="checkpoint",
+        cascade="all, delete-orphan",
+    )
 
 
 class Race(Base):
-    __tablename__ = 'races'
+    __tablename__ = "races"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     date: Mapped[datetime] = mapped_column(DateTime)
     is_active: Mapped[bool] = mapped_column(Boolean)
 
-    race_runners: Mapped[List["RaceRunner"]] = relationship(back_populates="race")
-    race_checkpoints: Mapped[List["RaceCheckpoint"]] = relationship("RaceCheckpoint", back_populates="race")
+    race_runners: Mapped[List["RaceRunner"]] = relationship(
+        "RaceRunner",
+        back_populates="race",
+        cascade="all, delete-orphan"
+
+    )
+    race_checkpoints: Mapped[List["RaceCheckpoint"]] = relationship(
+        "RaceCheckpoint",
+        back_populates="race",
+        cascade="all, delete-orphan",
+    )
+
+
+class Event(Base):
+    __tablename__ = "events"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    runner_id: Mapped[int] = mapped_column(ForeignKey("runners.id", ondelete="CASCADE"))
+    checkpoint_id: Mapped[int] = mapped_column(ForeignKey("checkpoints.id", ondelete="CASCADE"))
+    race_id: Mapped[int] = mapped_column(ForeignKey("races.id", ondelete="CASCADE"))
+    timestamp: Mapped[datetime] = mapped_column(DateTime)
+
+    runner: Mapped[Runner] = relationship(
+        "Runner",
+    )
+
+    checkpoint: Mapped[Checkpoint] = relationship(
+        "Checkpoint",
+    )
+
+    race: Mapped[Race] = relationship(
+        "Race"
+    )
 
 
 class RaceCheckpoint(Base):
-    __tablename__ = 'race_checkpoints'
-    race_id: Mapped[int] = mapped_column(ForeignKey('races.id'), primary_key=True, autoincrement=False)
-    checkpoint_id: Mapped[int] = mapped_column(ForeignKey('checkpoints.id'), primary_key=True, autoincrement=False)
+    __tablename__ = "race_checkpoints"
+    race_id: Mapped[int] = mapped_column(ForeignKey("races.id"), primary_key=True,
+                                         autoincrement=False)
+    checkpoint_id: Mapped[int] = mapped_column(ForeignKey("checkpoints.id"), primary_key=True,
+                                               autoincrement=False)
 
-    race: Mapped[Race] = relationship(Race, back_populates="race_checkpoints")
-    checkpoint: Mapped[Checkpoint] = relationship(Checkpoint, back_populates="race_checkpoints")
+    race: Mapped[Race] = relationship(
+        "Race",
+        back_populates="race_checkpoints",
+    )
+    checkpoint: Mapped[Checkpoint] = relationship(
+        "Checkpoint",
+        back_populates="race_checkpoints",
+    )
 
 
 class RaceRunner(Base):
-    __tablename__ = 'race_runners'
-    race_id: Mapped[int] = mapped_column(ForeignKey('races.id'), primary_key=True, autoincrement=False)
-    runner_id: Mapped[int] = mapped_column(ForeignKey('runners.id'), primary_key=True, autoincrement=False)
+    __tablename__ = "race_runners"
+    race_id: Mapped[int] = mapped_column(ForeignKey("races.id"), primary_key=True,
+                                         autoincrement=False)
+    runner_id: Mapped[int] = mapped_column(ForeignKey("runners.id"), primary_key=True,
+                                           autoincrement=False)
 
-    runner: Mapped[Runner] = relationship("Runner", back_populates="race_runners")
-    race: Mapped[Race] = relationship("Race", back_populates="race_runners")
-
-
+    runner: Mapped[Runner] = relationship(
+        "Runner",
+        back_populates="race_runners",
+    )
+    race: Mapped[Race] = relationship(
+        "Race",
+        back_populates="race_runners",
+    )
